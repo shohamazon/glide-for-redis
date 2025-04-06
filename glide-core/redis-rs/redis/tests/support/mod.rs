valkey-io/valkey-glide

Move ServerError into Value
Signed-off-by: Shoham Elias <shohame@amazon.com>

glide-core/redis-rs/redis/tests/support/mod.rs
@@ -611,6 +611,10 @@ where
             }
             Ok(())
         }
+        Value::ServerError(ref err) => match err.details() {
+            Some(details) => write!(writer, "-{} {details}\r\n", err.code()),
+            None => write!(writer, "-{}\r\n", err.code()),
+        },
     }
 }
 

shohamazon/glide-for-redis

glide-core/redis-rs/redis/tests/support/mod.rs
@@ -611,6 +611,10 @@ where
             }
             Ok(())
         }
+        Value::ServerError(ref err) => match err.details() {
+            Some(details) => write!(writer, "-{} {details}\r\n", err.err_code()),
+            None => write!(writer, "-{}\r\n", err.err_code()),
+        },
     }
 }
 